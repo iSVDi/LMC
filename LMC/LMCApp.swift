a78911b190677dd188f79518ee0edf7632947c59
ShDaniil <d.v.shestopalov@mail.ru>
2024-09-19 14:06:16 +0300

    create swiftui project

---
+++ LMC/LMCApp.swift
@@ -0,0 +1,17 @@
+//
+//  LMCApp.swift
+//  LMC
+//
+//  Created by daniil on 19.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct LMCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
